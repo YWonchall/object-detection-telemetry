2e072c6473308b971f6c641a8ad3ef6d3f5686ca
root <root@training-webide-9149c4-9149c4-webide-master-0.training-webide-9149c4-9149c4-webide-master.dev-kowteozo.svc.cluster.local>
2022-07-13 17:46:23 +0800

    init

---
+++ main/config.sh
@@ -0,0 +1,4 @@
+pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.9.0/index.html
+pip install mmdet
+pip install -r ../mmrotate-main/requirements/build.txt
+pip install -v -e ../mmrotate-main/.
